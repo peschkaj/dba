d8a07e890444fcf0df830cb80c93f5632fb842d8
Jeremiah Peschka <jeremiah@brentozar.com>
2013-02-01 11:48:44 -0800

    first commit, yo

---
+++ README.md
@@ -0,0 +1,12 @@
+# dba.sh
+
+It's a DBA simulator.
+
+## Setup
+
+`chmod +x dba.sh`
+
+## Execution
+
+Open a shell
+`./dba.sh`
---
+++ dba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while [ 1 ]; do
+	say "no";
+	sleep 5s;
+	say "bad idea"
+	sleep 10s;
+	say "that won't work in dee are";
+	sleep 7s;
+	say "no";
+	sleep 10s;
+	say "I'm not deploying that feature"
+	sleep 5s;
+done
