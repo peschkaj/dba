3335f0f046dec1af356003671358dc1d9b2488fd
Jeremiah Peschka <jeremiah@brentozar.com>
2013-02-03 13:11:16 -0800

    Added the magic/pain of bash looping

--- dba.sh
+++ dba.sh
@@ -9,63 +9,14 @@
 # Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+OLD_IFS=$IFS
+IFS=$'\n'
+Sayings=( $(cat "./sayings") )
+IFS=$OLD_IFS
+
+length=${#Sayings[@]};
+ 
 while [ 1 ]; do
-	say "no";
-	sleep 5s;
-	say "bad idea"
-	sleep 10s;
-	say "that won't work in dee are";
-	sleep 7s;
-	say "no";
-	sleep 10s;
-	say "I'm not deploying that feature"
-	sleep 5s;
-	say "Oh, that's cute, no"
-	sleep 2s;
-	say "I need RAID 10";
-	sleep 5s;
-	say "Our license doesn't cover that";
-	sleep 5s;
-	say "That's not best practices";
-	sleep 10s;
-	say "It's a network problem";
-	sleep 10s;
-	say "The index needs to be defragmented";
-	sleep 3s;
-	say "You don't have permissions to see that";
-	sleep 8s;
-	say "That would break replication";
-	sleep 5s;
-	say "It needs to be compressive, sargable, and performant";
-	sleep 10s;
-	say "I've never seen XML used like that. Tell me more."
-	sleep 2s
-	say "This is an application problem"
-	sleep 4s
-	say "Are you trying to increase the hardware budget?"
-	sleep 6s
-	say "Of course all columns should be nullable"
-	sleep 1s
-	say "Just because I wrote a cursor, doesn't mean you should"
-	sleep 5s
-	say "You can easily perform this join in the app layer"
-	sleep 3s
-	say "What's hard about caching?"
-	sleep 2s
-	say "If I don't have permissions, I don't see why you shold get permissions"
-	sleep 5s
-	say "Adam Machanic explained it to me. You're not capable of understanding it."
-	sleep 4s
-	say "While you were busy playing Minecraft, I was doing my job"
-	sleep 3s
-	say "It's hard to look this good and put up with you at the same time"
-	sleep 6s
-	say "No."
-	sleep 3s
-	say "I'm an MCDBA, trust me."
-	sleep 4s
-	say "Don't make me call the consultant."
-	sleep 2s
-	say "Did you forget the last time I said no?"
-	sleep 6s
+	say ${Sayings[$[($RANDOM % length)]]};
+	sleep $[($RANDOM % 3) + 1];
 done
